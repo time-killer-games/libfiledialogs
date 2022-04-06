1ab018127ff888d27684abc35b605238e43e9969
Samuel Venable <samuelvenable@hotmail.com>
2022-04-05 17:07:43 -0700

    Add files via upload

--- build.sh
+++ build.sh
@@ -1,7 +1,6 @@
 cd "${0%/*}"
 
 if [ `uname` = "Darwin" ]; then
-  sudo port install libsdl2
   clang++ "/opt/local/lib/libSDL2.a" "/opt/local/lib/libiconv.a" "ImFileDialog.cpp" "imgui.cpp" "imgui_impl_sdl.cpp" "imgui_impl_opengl2.cpp" "imgui_draw.cpp" "imgui_tables.cpp" "imgui_widgets.cpp" "filesystem.cpp" "filedialogs.cpp" "main.cpp" -o "filedialogs" -std=c++20 -Wno-deprecated-enum-enum-conversion -I. -DIMGUI_USE_WCHAR32 -I/opt/local/include -I/opt/local/include/SDL2 -std=c++20 -Wno-deprecated-enum-enum-conversion -ObjC++ -Wl,-framework,CoreAudio -Wl,-framework,AudioToolbox -Wl,-weak_framework,CoreHaptics -Wl,-weak_framework,GameController -Wl,-framework,ForceFeedback -lobjc -Wl,-framework,CoreVideo -Wl,-framework,Cocoa -Wl,-framework,Carbon -Wl,-framework,IOKit -Wl,-weak_framework,QuartzCore -Wl,-weak_framework,Metal -framework OpenGL -fPIC -arch arm64 -arch x86_64 -fPIC
 elif [ $(uname) = "Linux" ]; then
   g++ "ImFileDialog.cpp" "imgui.cpp" "imgui_impl_sdl.cpp" "imgui_impl_opengl2.cpp" "imgui_draw.cpp" "imgui_tables.cpp" "imgui_widgets.cpp" "filesystem.cpp" "filedialogs.cpp" "main.cpp" -o "filedialogs" -std=c++20 -Wno-deprecated-enum-enum-conversion -I. -DIMGUI_USE_WCHAR32 -static-libgcc -static-libstdc++ `pkg-config --cflags --libs sdl2 --static` -lGL -lpthread -no-pie -fPIC
