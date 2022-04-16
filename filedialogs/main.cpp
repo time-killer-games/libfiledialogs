8a41d977cbf27c2c1668be0feebf25dcdaa07e92
macintosh <macintosh@macintoshs-Mini.fios-router.home>
2022-04-16 03:39:39 -0400

    yay

--- filedialogs/main.cpp
+++ filedialogs/main.cpp
@@ -49,18 +49,18 @@ int main(int argc, const char **argv) {
   #endif
   if (argc <= 2) {
     return 0;
-  } else if (argc == 3 && strcmp(argv[1], "--get-open-filename") == 0) {
-    std::cout << get_open_filename(argv[2]) << std::endl;
-  } else if (argc == 5 && strcmp(argv[1], "--get-open-filename-ext") == 0) {
-    std::cout << get_open_filename_ext(argv[2], argv[3], argv[4]) << std::endl;
-  } else if (argc == 3 && strcmp(argv[1], "--get-open-filenames") == 0) {
-    std::cout << get_open_filenames(argv[2]) << std::endl;
-  } else if (argc == 5 && strcmp(argv[1], "--get-open-filenames-ext") == 0) {
-    std::cout << get_open_filenames_ext(argv[2], argv[3], argv[4]) << std::endl;
-  } else if (argc == 3 && strcmp(argv[1], "--get-save-filename") == 0) {
-    std::cout << get_save_filename(argv[1]) << std::endl;
-  } else if (argc == 5 && strcmp(argv[1], "--get-save-filename-ext") == 0) {
-    std::cout << get_save_filename_ext(argv[2], argv[3], argv[4]) << std::endl;
+  } else if (argc == 4 && strcmp(argv[1], "--get-open-filename") == 0) {
+    std::cout << get_open_filename(argv[2], argv[3]) << std::endl;
+  } else if (argc == 6 && strcmp(argv[1], "--get-open-filename-ext") == 0) {
+    std::cout << get_open_filename_ext(argv[2], argv[3], argv[4], argv[5]) << std::endl;
+  } else if (argc == 4 && strcmp(argv[1], "--get-open-filenames") == 0) {
+    std::cout << get_open_filenames(argv[2], argv[3]) << std::endl;
+  } else if (argc == 6 && strcmp(argv[1], "--get-open-filenames-ext") == 0) {
+    std::cout << get_open_filenames_ext(argv[2], argv[3], argv[4], argv[5]) << std::endl;
+  } else if (argc == 4 && strcmp(argv[1], "--get-save-filename") == 0) {
+    std::cout << get_save_filename(argv[2], argv[3]) << std::endl;
+  } else if (argc == 6 && strcmp(argv[1], "--get-save-filename-ext") == 0) {
+    std::cout << get_save_filename_ext(argv[2], argv[3], argv[4], argv[5]) << std::endl;
   } else if (argc == 3 && strcmp(argv[1], "--get-directory") == 0) {
     std::cout << get_directory(argv[2]) << std::endl;
   } else if (argc == 4 && strcmp(argv[1], "--get-directory-alt") == 0) {
