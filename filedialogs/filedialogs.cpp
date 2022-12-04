465c89049c0ab81c80cc0640bf4d368e2db73cc4
Samuel Venable <samuelvenable@hotmail.com>
2022-12-04 12:21:34 -0500

    Update filedialogs.cpp

--- filedialogs/filedialogs.cpp
+++ filedialogs/filedialogs.cpp
@@ -344,7 +344,7 @@ namespace {
     IMGUI_CHECKVERSION();
     ImGui::CreateContext(); ifd_load_fonts();
     if (ngs::fs::environment_get_variable("IMGUI_FONT_SIZE").empty())
-    ngs::fs::environment_set_variable("IMGUI_FONT_SIZE", std::to_string(16));
+    ngs::fs::environment_set_variable("IMGUI_FONT_SIZE", std::to_string(20));
     ImGuiIO& io = ImGui::GetIO(); (void)io; ImFontConfig config; io.IniFilename = nullptr;
     config.MergeMode = true; ImFont *font = nullptr; ImWchar ranges[] = { 0x0020, 0xFFFF, 0 }; 
     float fontSize = (float)strtod(ngs::fs::environment_get_variable("IMGUI_FONT_SIZE").c_str(), nullptr);
