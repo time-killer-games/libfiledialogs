d3b39c416b31f9c028a7dba52c7d0df307d528e7
Samuel Venable <samuelvenable@hotmail.com>
2024-03-02 21:39:45 -0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-# this build script is intended for windows-only; allows all fonts and other files to be packed into a single execuable file.
+# this build script is intended for windows-only; packs all fonts and other files and compiles into a single executable file.
 # make sure you build the visual c++ project first found in the "filedialogs" sub-directory of this repository's source tree.
 # run "filedialogs/build.sh" instead of this script to build on all non-windows platforms: macos, linux, bsd, sunos, android.
 # android building requires the cxxdroid app from google play: https://play.google.com/store/apps/details?id=ru.iiec.cxxdroid
