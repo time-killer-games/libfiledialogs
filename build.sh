b7f4ead4f21b38edbe423ee7aae03393e692f128
Samuel Venable <samuelvenable@hotmail.com>
2024-03-11 00:58:48 -0400

    Update build.sh

--- build.sh
+++ build.sh
@@ -15,8 +15,11 @@ elif [ $(uname) = "OpenBSD" ]; then
 elif [ $(uname) = "SunOS" ]; then
   "./filedialogs/build (SDL Renderer).sh";
 else
-  echo "make sure you build the visual c++ project first found in the \"filedialogs\" sub-directory of this repository's source tree.";
-  windres resources.rc -o resources.o;
-  g++ main.cpp -o filedialogs.exe apiprocess/process.cpp resources.o -std=c++17 -static-libgcc -static-libstdc++ -static -lntdll;
-  rm -f resources.o;
+  if [ -f "./filedialogs/filedialogs.exe" ]
+    windres resources.rc -o resources.o;
+    g++ main.cpp -o filedialogs.exe apiprocess/process.cpp resources.o -std=c++17 -static-libgcc -static-libstdc++ -static -lntdll;
+    rm -f resources.o;
+  else
+    echo "make sure you build the visual c++ project first found in the \"filedialogs\" sub-directory of this repository's source tree.";
+  fi
 fi
