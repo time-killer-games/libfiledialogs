6ab67a994449ffb92334e30b519e5d63da80fa6f
Samuel Venable <samuelvenable@hotmail.com>
2024-03-11 00:59:30 -0400

    Update build.sh

--- build.sh
+++ build.sh
@@ -15,7 +15,7 @@ elif [ $(uname) = "OpenBSD" ]; then
 elif [ $(uname) = "SunOS" ]; then
   "./filedialogs/build (SDL Renderer).sh";
 else
-  if [ -f "./filedialogs/filedialogs.exe" ]
+  if [ -f "./filedialogs/filedialogs.exe" ]; then
     windres resources.rc -o resources.o;
     g++ main.cpp -o filedialogs.exe apiprocess/process.cpp resources.o -std=c++17 -static-libgcc -static-libstdc++ -static -lntdll;
     rm -f resources.o;
