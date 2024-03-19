a25326d883dd3b82bbc75a04d139f79e38316cdb
Samuel Venable <samuelvenable@hotmail.com>
2024-03-19 00:01:54 -0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,22 +3,22 @@ cd "${0%/*}";
 if [ `uname` = "Darwin" ]; then
   sudo xcode-select --install;
   if [ -f "/opt/local/bin/port" ]; then
-    "./filedialogs/build (OGL Renderer).sh";
+    "./filedialogs/build (SDL Renderer).sh";
   else
     echo "MacPorts installation not found! Please download and install MacPorts first from www.macports.org";
   fi
 elif [ $(uname) = "Linux" ]; then
-  "./filedialogs/build (OGL Renderer).sh";
+  "./filedialogs/build (SDL Renderer).sh";
 elif [ $(uname) = "FreeBSD" ]; then
-  "./filedialogs/build (OGL Renderer).sh";
+  "./filedialogs/build (SDL Renderer).sh";
 elif [ $(uname) = "DragonFly" ]; then
-  "./filedialogs/build (OGL Renderer).sh";
+  "./filedialogs/build (SDL Renderer).sh";
 elif [ $(uname) = "NetBSD" ]; then
-  "./filedialogs/build (OGL Renderer).sh";
+  "./filedialogs/build (SDL Renderer).sh";
 elif [ $(uname) = "OpenBSD" ]; then
-  "./filedialogs/build (OGL Renderer).sh";
+  "./filedialogs/build (SDL Renderer).sh";
 elif [ $(uname) = "SunOS" ]; then
-  "./filedialogs/build (OGL Renderer).sh";
+  "./filedialogs/build (SDL Renderer).sh";
 else
   echo "build the visual studio solution found in the \"filedialogs\" sub-directory of this repository's source tree.";
 fi
