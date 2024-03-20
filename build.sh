feeb2194f0d796ec3ec2cb3720c03164131c6f8d
Samuel Venable <samuelvenable@hotmail.com>
2024-03-20 12:10:13 -0400

    Update build.sh

--- build.sh
+++ build.sh
@@ -8,17 +8,17 @@ if [ `uname` = "Darwin" ]; then
     echo "MacPorts installation not found! Please download and install MacPorts first from www.macports.org";
   fi
 elif [ $(uname) = "Linux" ]; then
-  "./filedialogs/build (SDL Renderer).sh";
+  "./filedialogs/build (OGL Renderer).sh";
 elif [ $(uname) = "FreeBSD" ]; then
-  "./filedialogs/build (SDL Renderer).sh";
+  "./filedialogs/build (OGL Renderer).sh";
 elif [ $(uname) = "DragonFly" ]; then
-  "./filedialogs/build (SDL Renderer).sh";
+  "./filedialogs/build (OGL Renderer).sh";
 elif [ $(uname) = "NetBSD" ]; then
-  "./filedialogs/build (SDL Renderer).sh";
+  "./filedialogs/build (OGL Renderer).sh";
 elif [ $(uname) = "OpenBSD" ]; then
-  "./filedialogs/build (SDL Renderer).sh";
+  "./filedialogs/build (OGL Renderer).sh";
 elif [ $(uname) = "SunOS" ]; then
-  "./filedialogs/build (SDL Renderer).sh";
+  "./filedialogs/build (OGL Renderer).sh";
 else
   echo "build the visual studio solution found in the \"filedialogs\" sub-directory of this repository's source tree.";
 fi
